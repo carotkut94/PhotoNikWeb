f360f043d18d123f9ce2e4dcf621e55332eedf39
deathcode <carotkut12@gmail.com>
2019-04-10 11:53:46 +0530

    changes for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const app = require("./app");
 
-const PORT = 30023;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(PORT);
